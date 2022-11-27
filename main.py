79d0fffa935bf363aaf70b4b03d226c87682fcaa
Harry Youd <harry@harryyoud.co.uk>
2022-11-27 19:58:26 +0000

    Update

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 import os
+import os.path
 
 os.makedirs("sites-available", exist_ok=True)
 os.makedirs("sites-enabled", exist_ok=True)
 [f.unlink() for f in Path("sites-available").glob("*") if f.is_file()]
-[f.unlink() for f in Path("sites-enabled").glob("*") if f.is_file()]
+[f.unlink() for f in Path("sites-enabled").glob("*") if f.is_file() or f.is_symlink()]
 
 environment = Environment(loader=FileSystemLoader("templates/"))
-for name in environment.list_templates(None, lambda x: x.endswith(".conf.jinja2") and
-                                       not x.startswith("_")):
+templates = environment.list_templates(None, lambda x: x.endswith(".conf.jinja2") and
+                                       not os.path.basename(x).startswith("_"))
+
+print(f"Found {len(templates)} templates to render")
+
+for name in templates:
     template = environment.get_template(name)
     out_name = name.replace('.jinja2', '')
 
     with open(f'sites-available/{out_name}', 'w') as f:
         f.write(template.render())
         os.symlink(f"../sites-available/{out_name}", f"sites-enabled/{out_name}")
+        print(f" => Processed {out_name}")
+
+print("All done!")
